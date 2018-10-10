b30198183f4db4b21ad915d5b6dec3ed80eafcce
simon8029 <simon.lau@msn.com>
2018-10-10 17:15:59 -0400

    mongodb test files setup

---
+++ tests/unit/mongodb/mongodb.module.spec.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var MongoDBModule = require('../../../modules/mongodb/mongodb.modules');
+
+describe('MongoDBModule', () => {
+	describe('mongodb.module file', () => {
+		it('should test first behavior', () => {});
+	});
+});
---
+++ tests/unit/mongodb/mongodb.util.specs.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var MongoDBUtil = require('../../../Utils/mongodb/mongodb.util');
+
+describe('MongoDBUtil', () => {
+	describe('mongodb.util file', () => {
+		it('should test first behavior', () => {});
+	});
+});
