31b5191166a2d572e448eb052a44a6cd91d64e8b
simon8029 <simon.lau@msn.com>
2018-10-10 18:30:18 -0400

    Setup mongoose connect to mongodb.

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var MongoDBUtil = require('./modules/mongodb/mongodb.module').MongoDBUtil;
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
+
+MongoDBUtil.init();
 
 app.get('/', function(req, res) {
 	console.log(`__dirname:`, __dirname);
